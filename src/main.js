65ca1063b7e90a4bf75645d367f01eaceae96fe8
ThiagoHenriquesPessoa <thiagokxl5@gmail.com>
2023-10-14 19:48:11 -0300

    configurando a home

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(app)
+
+app.use(router)
+
+App.mount('#app')
